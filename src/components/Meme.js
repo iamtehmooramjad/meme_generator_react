bb4f41c31fd832198e6d9b7d1c54ae10e52ee938
AR Developer <AR.Developer@accessretailpk.local>
2022-08-12 11:51:46 +0500

    States in meme addede

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,21 +2,32 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme(){
-    let url
+    const [allMemeImages,setAllMemeImages] = React.useState(memesData.data.memes)
+
+    const [meme,setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
+    })
+
+
     function getRandomMeme() {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        console.log(url)
+
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        let url = allMemeImages[randomNumber].url
+        setMeme(prevMeme => ({
+                ...prevMeme,
+                randomImage : url
+        }))
     }
     return(
         <main>
-            <p>{url}</p>
             <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
                 <button className="form--button" onClick={getRandomMeme}>Get a new meme image  🖼</button>
             </div>
+            <img src={meme.randomImage} alt="Meme" className="meme--image" />
         </main>
     )
 }
