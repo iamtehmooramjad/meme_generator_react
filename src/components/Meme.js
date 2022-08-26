eb7d26ae2e2deb7248a5517f149ce263abea6d76
Tehmoor Amjad <AR.Developer@accessretailpk.local>
2022-08-26 17:23:37 +0500

    Completed

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,33 +1,99 @@
-import React from "react";
-import memesData from "../memesData";
+import React from "react"
 
-export default function Meme(){
-    const [allMemeImages,setAllMemeImages] = React.useState(memesData.data.memes)
 
-    const [meme,setMeme] = React.useState({
-        topText: '',
-        bottomText: '',
-        randomImage: 'http://i.imgflip.com/1bij.jpg'
+/**
+ useEffect takes a function as its parameter. If that function
+ returns something, it needs to be a cleanup function. Otherwise,
+ it should return nothing. If we make it an async function, it
+ automatically retuns a promise instead of a function or nothing.
+ Therefore, if you want to use async operations inside of useEffect,
+ you need to define the function separately inside of the callback
+ function, as seen below:
+ */
+
+export default function Meme() {
+
+/*    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res=>res.json())
+            .then(data=>{
+                setAllMemes(data.data.memes)
+            })
+            .catch(e=>console.log(e));
+    },[]);*/
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+
+        }
+        getMemes()
+
+        return () => {
+
+        }
+    }, [])
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
     })
+    const [allMemes, setAllMemes] = React.useState([])
 
 
-    function getRandomMeme() {
+    function getMemeImage() {
 
-        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
-        let url = allMemeImages[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme=> ({
+            ...prevMeme,
+            randomImage: url
+        }))
+
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
         setMeme(prevMeme => ({
-                ...prevMeme,
-                randomImage : url
+            ...prevMeme,
+            [name]: value
         }))
     }
-    return(
+
+    return (
         <main>
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button" onClick={getRandomMeme}>Get a new meme image  🖼</button>
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
             </div>
-            <img src={meme.randomImage} alt="Meme" className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
